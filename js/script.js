a4c93cf8128a11f5ceda2309c229f96dfb1f2f11
Valerio <valeriodevivo90@gmail.com>
2021-01-26 17:12:29 +0100

    Next and Prev done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var app = new Vue ({
+    el: '#slider-container',
+    data: {
+        counter: 0,
+        img: [
+            'background-image: url(css/img/1.jpg)',
+            'background-image: url(css/img/2.jpg)',
+            'background-image: url(css/img/3.jpg)',
+            'background-image: url(css/img/4.jpg)'
+        ],
+    },
+    methods: {
+        next(){
+            (this.counter == this.img.length - 1) ? this.counter = 0 : this.counter++;
+        },
+
+        prev(){
+            (this.counter == 0) ? this.counter = (this.img.length - 1) : this.counter--;
+        }
+    }
+});
